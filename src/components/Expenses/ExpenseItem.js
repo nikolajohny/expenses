19f91b40c0f6011249a2a6f31296dbf13b500a05
Nikola Djakovic <n.djakovic@vegait.rs>
2023-01-24 12:40:00 +0100

    Update JS, change function to const

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
 import "../Expenses/ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
