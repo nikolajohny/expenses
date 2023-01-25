7dcf593b21d78af6d8bcfb9cd9793dd53d2ac243
Nikola Djakovic <n.djakovic@vegait.rs>
2023-01-25 13:17:58 +0100

    Use state, generate information of items

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../NewExpense/ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  // ne mora se 3 puta odvojeno pozivati useState, vec moze samo jednom kao u primeru ispod, prvi nacin se cesce vidi u praski nego drugi nacin.
+
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setenteredAmount] = useState("");
+  const [enteredDate, setenteredDate] = useState("");
+
+  //   const [userInput, setUserInpuit] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: ''
+  //   })
+
+  const titleChangeHandler = (event) => {
+    // prvi nacin
+
+    setEnteredTitle(event.target.value);
+
+    // drugi nacin
+
+    // setUserInpuit({
+    //     ...userInput,
+    //     enteredTitle: event.target.value,
+    // })
+
+    // treci nacin
+
+    // setUserInpuit((prevState) => {
+    //     return {...prevState, enteredTitle: event.target.value };
+    // })
+  };
+
+  const amountChangeHandler = (event) => {
+    setenteredAmount(event.target.value);
+    // setUserInpuit({
+    //     ...userInput,
+    //     enteredAmount: event.target.value,
+    // })
+    // setUserInpuit((prevState) => {
+    //     return {...prevState, enteredAmount: event.target.value };
+    // })
+  };
+
+  const dateChangeHandler = (event) => {
+    setenteredDate(event.target.value);
+    // setUserInpuit({
+    //     ...userInput,
+    //     enteredDate: event.target.value,
+    // })
+    // setUserInpuit((prevState) => {
+    //     return {...prevState, enteredDate: event.target.value };
+    // })
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+	props.onSaveExpenseDate(expenseDate);
+    setEnteredTitle("");
+    setenteredAmount("");
+    setenteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31"/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,17 @@ import React from "react";
 import "../NewExpense/NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDatehandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        props.onAddExpense(expenseData)
+    }
+
   return <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseDate={saveExpenseDatehandler} />
   </div>;
 };
 
